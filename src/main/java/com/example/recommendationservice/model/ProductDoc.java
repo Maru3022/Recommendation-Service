84588f3e9b8a595f725fc56adfd1edfd53ae2273
Maru <pdijsj2992@gmail.com>
2026-02-15 20:29:42 +0300

    fix: replace JPA annotations with Spring Data in ProductDoc

--- src/main/java/com/example/recommendationservice/model/ProductDoc.java
+++ src/main/java/com/example/recommendationservice/model/ProductDoc.java
@@ -1,20 +1,27 @@
 package com.example.recommendationservice.model;
 
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 @Document(indexName = "products")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ProductDoc {
 
     @Id
     private String id;
 
-    @Field
+    @Field(type = FieldType.Text)
     private String name;
+
+    @Field(type = FieldType.Keyword)
     private String category;
+
+    private Double price;
 }
