9e19263b4a0e202c530c79b0de8b66003a8608c4
Maru <pdijsj2992@gmail.com>
2026-02-17 16:08:04 +0300

    Revert "feat: implement Elasticsearch repositories"
    This reverts commit 4d3e6733f31733918a1dc0935f49f344869c8556.

--- src/main/java/com/example/recommendationservice/repository/ActionRepository.java
+++ src/main/java/com/example/recommendationservice/repository/ActionRepository.java
@@ -1,14 +1,4 @@
 package com.example.recommendationservice.repository;
 
-import com.example.recommendationservice.model.ProductDoc;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-@Repository
-public interface ActionRepository
-        extends ElasticsearchRepository<ProductDoc, String> {
-
-    List<ProductDoc> findByCategory(String category);
+public class ActionRepository {
 }
--- src/main/java/com/example/recommendationservice/repository/ProductSearchRepository.java
+++ src/main/java/com/example/recommendationservice/repository/ProductSearchRepository.java
@@ -1,13 +1,4 @@
 package com.example.recommendationservice.repository;
 
-import com.example.recommendationservice.model.ProductDoc;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-@Repository
-public interface ProductSearchRepository
-        extends ElasticsearchRepository<ProductDoc, String> {
-    List<ProductDoc> findByCategory(String category);
+public class ProductSearchRepository {
 }
